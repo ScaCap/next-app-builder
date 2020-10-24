040c53e304cb2d25ce32be0f9ee9c370ae2e1fb1
Anna Gruber <anna@scalable.capital>
2020-10-24 21:48:35 +0200

    start cleaning

--- src/index.tsx
+++ src/index.tsx
@@ -3,118 +3,116 @@ import App from 'next/app';
 import { AppContext } from 'next/dist/pages/_app';
 
 export type NextAppMiddleware<T = Record<string, unknown>> = {
-    name?: string;
-    getInitialProps?(appContext: AppContext): T | Promise<T>;
-    Component?: FunctionComponent<T>;
-    componentDidCatch?(error: Error, errorInfo: ErrorInfo): App['componentDidCatch'];
+  name?: string;
+  getInitialProps?(appContext: AppContext): T | Promise<T>;
+  Component?: FunctionComponent<T>;
+  componentDidCatch?(error: Error, errorInfo: ErrorInfo): App['componentDidCatch'];
 };
 
 type NextAppBuilderOptions = {
-    middleware: NextAppMiddleware[];
+  middleware: NextAppMiddleware[];
 };
 
 type NextAppMiddlewareBuilder = (options: NextAppBuilderOptions) => typeof App;
 
-const executeComponentDidCatchMiddleware = (allMiddleware, error, errorInfo) =>
-    allMiddleware.forEach(({ componentDidCatch }) => {
-        if (componentDidCatch) {
-            componentDidCatch(error, errorInfo);
-        }
-    });
+type ExecuteComponentDidCatchMiddleware = (
+  allMiddlewarec,
+  error: Error,
+  _errorInfo: ErrorInfo
+) => void;
 
-const renderPage = (
-    allMiddleware,
-    { Component: PageComponent, pageProps: { middlewareProps, ...props } }
-) =>
-    allMiddleware
-        .filter(({ Component: MiddlewareComponent }) => !!MiddlewareComponent)
-        .reduceRight(
-            (nestedElement, { Component: MiddlewareComponent, id }) => (
-                <MiddlewareComponent {...props} {...middlewareProps[id]}>
-                    {nestedElement}
-                </MiddlewareComponent>
-            ),
-            <PageComponent {...props} />
-        );
+const executeComponentDidCatchMiddleware: ExecuteComponentDidCatchMiddleware = (allMiddleware, error, errorInfo) =>
+  allMiddleware.forEach(({ componentDidCatch }) => {
+    if (componentDidCatch) {
+      componentDidCatch(error, errorInfo);
+    }
+  });
+
+const renderPage = (allMiddleware, { Component: PageComponent, pageProps: { middlewareProps, ...props } }) =>
+  allMiddleware
+    .filter(({ Component: MiddlewareComponent }) => !!MiddlewareComponent)
+    .reduceRight(
+      (nestedElement, { Component: MiddlewareComponent, id }) => (
+        <MiddlewareComponent {...props} {...middlewareProps[id]}>
+          {nestedElement}
+        </MiddlewareComponent>
+      ),
+      <PageComponent {...props} />
+    );
 
 const nextAppBuilder: NextAppMiddlewareBuilder = ({ middleware = [] }) => {
-    const allMiddleware = middleware.map((singleMiddleware, index) => ({
-        ...singleMiddleware,
-        id: `nextAppMiddleware-${index}`
-    }));
+  const allMiddleware = middleware.map((singleMiddleware, index) => ({
+    ...singleMiddleware,
+    id: `nextAppMiddleware-${index}`
+  }));
 
-    class NextAppMiddlewareComponent extends App {
-        static async getInitialProps({ Component, ctx, router }) {
-            let pageProps = {};
-            const { AppTree } = ctx;
-            const extendPageProps = props => {
-                pageProps = {
-                    ...pageProps,
-                    ...props
-                };
-            };
-            if (Component.getInitialProps) {
-                extendPageProps(await Component.getInitialProps(ctx));
-            }
+  class NextAppMiddlewareComponent extends App {
+    static async getInitialProps({ Component, ctx, router }): Promise<{pageProps: any}> {
+      let pageProps = {};
+      const { AppTree } = ctx;
+      const extendPageProps = props => {
+        pageProps = {
+          ...pageProps,
+          ...props
+        };
+      };
+      if (Component.getInitialProps) {
+        extendPageProps(await Component.getInitialProps(ctx));
+      }
 
-            let middlewareProps;
+      let middlewareProps;
 
-            const InternalAppTree = props => {
-                const enhancedPageProps = { ...pageProps, middlewareProps, ...props };
-                return <AppTree pageProps={enhancedPageProps} />;
-            };
+      const InternalAppTree = props => {
+        const enhancedPageProps = { ...pageProps, middlewareProps, ...props };
+        return <AppTree pageProps={enhancedPageProps} />;
+      };
 
-            const allInitialProps = await Promise.all(
-                allMiddleware.map(async ({ getInitialProps, id, name }) => {
-                    let initialProps = {};
-                    if (getInitialProps) {
-                        try {
-                            initialProps = await getInitialProps({
-                                Component,
-                                router,
-                                ctx,
-                                AppTree: InternalAppTree
-                            });
-                        } catch (error) {
-                            console.warn(
-                                `getInitialProps failed for middlware with name ${
-                                    name || 'unnamed'
-                                }`,
-                                error
-                            );
-                        }
-                    }
-                    return { initialProps, id };
-                })
-            );
-
-            middlewareProps = allInitialProps.reduce(
-                (props, { id, initialProps }) => ({
-                    ...props,
-                    [id]: initialProps
-                }),
-                {}
-            );
-
-            extendPageProps({ middlewareProps });
-            return { pageProps };
-        }
-
-        componentDidCatch(error, errorInfo) {
-            executeComponentDidCatchMiddleware(allMiddleware, error, errorInfo);
-            // This is needed to render errors correctly in development / production
-            super.componentDidCatch(error, errorInfo);
-        }
-
-        render() {
-            const { Component, pageProps, ...otherProps } = this.props;
-            return renderPage(allMiddleware, {
+      const allInitialProps = await Promise.all(
+        allMiddleware.map(async ({ getInitialProps, id, name }) => {
+          let initialProps = {};
+          if (getInitialProps) {
+            try {
+              initialProps = await getInitialProps({
                 Component,
-                pageProps: { ...pageProps, ...otherProps }
-            });
-        }
+                router,
+                ctx,
+                AppTree: InternalAppTree
+              });
+            } catch (error) {
+              console.warn(`getInitialProps failed for middleware with name ${name || 'unnamed'}`, error);
+            }
+          }
+          return { initialProps, id };
+        })
+      );
+
+      middlewareProps = allInitialProps.reduce(
+        (props, { id, initialProps }) => ({
+          ...props,
+          [id]: initialProps
+        }),
+        {}
+      );
+
+      extendPageProps({ middlewareProps });
+      return { pageProps };
     }
-    return NextAppMiddlewareComponent;
+
+    componentDidCatch(error, errorInfo): void {
+      executeComponentDidCatchMiddleware(allMiddleware, error, errorInfo);
+      // This is needed to render errors correctly in development / production
+      super.componentDidCatch(error, errorInfo);
+    }
+
+    render(): JSX.Element {
+      const { Component, pageProps, ...otherProps } = this.props;
+      return renderPage(allMiddleware, {
+        Component,
+        pageProps: { ...pageProps, ...otherProps }
+      });
+    }
+  }
+  return NextAppMiddlewareComponent;
 };
 
 export default nextAppBuilder;
